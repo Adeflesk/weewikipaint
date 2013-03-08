19d389decf56df8864806760ad328be3634e2cb8
Adrian Corsini <glenflesk@gmail.com>
2013-03-08 23:32:30 +0000

    Initial support for lint

--- Jakefile.js
+++ Jakefile.js
@@ -1,13 +1,9 @@
 //noinspection JSCheckFunctionSignatures
-task("default", [], function(){
-    console.log("Default");
+task("default", ["lint"]);
+
+desc("Lint everything");
+task("lint", [], function(){
+    var lint = require("./build/lint/lint_runner.js");
+    lint.validateFile("Jakefile.js", {}, {});
 });
 
-desc("Example!");
-task("example",["dependency"], function(){
-    console.log("example task");
-});
-
-task("dependency", function(){
-    console.log("dependency");
-});
