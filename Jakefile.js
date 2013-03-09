af9be31cb3b6a291919f2a13dd899163059b4b67
Adrian Corsini <glenflesk@gmail.com>
2013-03-09 00:19:53 +0000

    Set up strict lint options

--- Jakefile.js
+++ Jakefile.js
@@ -1,9 +1,41 @@
-//noinspection JSCheckFunctionSignatures
+/*global desc, jake, task, fail, complete, require */
+(function() {
+'use strict';
+
 task("default", ["lint"]);
 
 desc("Lint everything");
 task("lint", [], function(){
     var lint = require("./build/lint/lint_runner.js");
-    lint.validateFile("Jakefile.js", {}, {});
+
+    var files = new jake.FileList();
+    files.include("**/*.js");
+    files.exclude("node_modules");
+
+
+
+    var options = nodeLintOptions();
+    lint.validateFileList(files.toArray(),options, {});
 });
 
+function nodeLintOptions() {
+
+    return {
+        bitwise: true,
+        curly: false,
+        eqeqeq: true,
+        forin: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        noempty: true,
+        nonew: true,
+        regexp: true,
+        undef: true,
+        strict: true,
+        trailing: true,
+        node: true
+    };
+}
+}());
